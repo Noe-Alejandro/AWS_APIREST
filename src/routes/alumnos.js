f031d08dc3300380006e7f7c49032767b1a82201
Noeshi <A18016311@alumnos.uady.mx>
2022-11-20 18:56:22 -0600

    Adding some endpoint

--- src/routes/alumnos.js
+++ src/routes/alumnos.js
@@ -1,17 +1,28 @@
 const { Router } = require('express');
 const router = Router();
+const _ = require('underscore');
 const alumnos = require('../sample.json');
+var alumnosIndex = alumnos.length;
 
 router.get('/alumnos', (req, res) =>{
     res.json(alumnos);
 });
 
+router.get('/alumnos/:id', (req, res) =>{
+    const { id } = req.params;
+    _.each(alumnos, (alumno, i) =>{
+        if(alumno.id == id){
+            res.json(alumno);
+        }
+    });
+});
+
 router.post('/alumnos', (req, res) =>{
     const { nombres, apellidos, matricula, promedio} = req.body;
     if(nombres && apellidos && matricula && promedio){
-        const id = alumnos.length + 1;
+        alumnosIndex += 1;
+        const id = alumnosIndex;
         const newAlumno = {id, ...req.body};
-        console.log(newAlumno);
         alumnos.push(newAlumno);
         res.status(201).json(alumnos);
     }else{
@@ -19,9 +30,34 @@ router.post('/alumnos', (req, res) =>{
     }
 });
 
+router.put('/alumnos/:id', (req, res) =>{
+    const { id } = req.params;
+    const { nombres, apellidos, matricula, promedio} = req.body;
+    if(nombres && apellidos && matricula && promedio){
+        _.each(alumnos, (alumno, i) =>{
+            if(alumno.id == id){
+                alumno.nombres = nombres;
+                alumno.apellidos = apellidos;
+                alumno.matricula = matricula;
+                alumno.promedio = promedio;
+                res.json({msg: 'Student updated'});
+            }
+        });
+    }else{
+        res.status(404).json({error: 'There was an error'});
+    }
+});
+
 router.delete('/alumnos/:id', (req, res) =>{
-    console.log(req.params);
-    res.send('Deleted');
+    const { id } = req.params;
+    var index = 0;
+    _.each(alumnos, (alumno, i) =>{
+        if(alumno.id == id){
+            index = i;
+            res.json({msg: 'Student deleted'});
+        }
+    });
+    alumnos.splice(index, 1);
 });
 
 module.exports = router;
