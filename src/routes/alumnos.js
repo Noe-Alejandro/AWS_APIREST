53e67a65e8e30a8612a00a3644834562058bddf0
Noeshi <A18016311@alumnos.uady.mx>
2022-11-21 12:15:34 -0600

    Adding function to validate body values and params

--- src/routes/alumnos.js
+++ src/routes/alumnos.js
@@ -1,18 +1,23 @@
 const { Router } = require('express');
+const { isString } = require('underscore');
 const router = Router();
 const _ = require('underscore');
 const alumnos = require('../sample.json');
 var alumnosIndex = alumnos.length;
 
 router.get('/alumnos', (req, res) =>{
-    res.json(alumnos);
+    res.status(200).json(alumnos);
 });
 
 router.get('/alumnos/:id', (req, res) =>{
     const { id } = req.params;
+    if(!IsOnlyNumber(id)){
+        res.status(400).json({error: 'Invalid id'});
+        return;
+    }
     for(var i = 0; i<alumnos.length; i++){
         if(alumnos[i].id == id){
-            res.json(alumnos[i]);
+            res.status(200).json(alumnos[i]);
             return;
         }
     }
@@ -21,39 +26,58 @@ router.get('/alumnos/:id', (req, res) =>{
 
 router.post('/alumnos', (req, res) =>{
     const { nombres, apellidos, matricula, promedio} = req.body;
-    if(nombres && apellidos && matricula && promedio){
-        alumnosIndex += 1;
-        const id = alumnosIndex;
-        const newAlumno = {id, ...req.body};
-        alumnos.push(newAlumno);
-        res.status(201).json(alumnos);
-    }else{
+    if(!IsNotNull([nombres, apellidos, matricula, promedio])){
         res.status(400).json({error: 'Invalid request'});
+        return;
     }
+    if(!IsNumberType([promedio]) || !IsStringType([nombres, apellidos, matricula])){
+        res.status(400).json({error: 'Invalid data type in request'});
+        return;
+    }
+    
+    alumnosIndex += 1;
+    const id = alumnosIndex;
+    const newAlumno = {id, ...req.body};
+    alumnos.push(newAlumno);
+    res.status(201).json(alumnos);
 });
 
 router.put('/alumnos/:id', (req, res) =>{
     const { id } = req.params;
     const { nombres, apellidos, matricula, promedio} = req.body;
-    if(nombres && apellidos && matricula && promedio){
-        for(var i = 0; i<alumnos.length; i++){
-            if(alumnos[i].id == id){
-                alumnos[i].nombres = nombres;
-                alumnos[i].apellidos = apellidos;
-                alumnos[i].matricula = matricula;
-                alumnos[i].promedio = promedio;
-                res.status(201).json({msg: 'Student updated'});
-                return;
-            }
-        }
-        res.status(404).json({error: 'Student not found'});
-    }else{
-        res.status(400).json({error: 'There was an error'});
+    if(!IsOnlyNumber(id)){
+        res.status(400).json({error: 'Invalid id'});
+        return;
     }
+    if(!IsNotNull([nombres, apellidos, matricula, promedio])){
+        res.status(400).json({error: 'Invalid request'});
+        return;
+    }
+    if(!IsNumberType([promedio]) || !IsStringType([nombres, apellidos, matricula])){
+        res.status(400).json({error: 'Invalid data type in request'});
+        return;
+    }
+
+    for(var i = 0; i<alumnos.length; i++){
+        if(alumnos[i].id == id){
+            alumnos[i].nombres = nombres;
+            alumnos[i].apellidos = apellidos;
+            alumnos[i].matricula = matricula;
+            alumnos[i].promedio = promedio;
+            res.status(201).json({msg: 'Student updated'});
+            return;
+        }
+    }
+    res.status(404).json({error: 'Student not found'});
 });
 
 router.delete('/alumnos/:id', (req, res) =>{
     const { id } = req.params;
+    if(!IsOnlyNumber(id)){
+        res.status(400).json({error: 'Invalid id'});
+        return;
+    }
+    
     for(var i = 0; i<alumnos.length; i++){
         if(alumnos[i].id == id){
             alumnos.splice(i, 1);
@@ -63,5 +87,36 @@ router.delete('/alumnos/:id', (req, res) =>{
     }
     res.status(404).json({error : 'Student not found'});
 });
+
+function IsOnlyNumber(str){
+    return /^[0-9]+$/.test(str);
+}
+
+function IsNotNull(array){
+    for(var i = 0; i<array.length; i++){
+        if(array[i] == null){
+            return false;
+        }
+    }
+    return true;
+}
+
+function IsNumberType(array){
+    for(var i = 0; i<array.length; i++){
+        if(typeof(array[i]) != "number"){
+            return false;
+        } 
+    }
+    return true;
+}
+
+function IsStringType(array){
+    for(var i = 0; i<array.length; i++){
+        if(typeof(array[i]) != "string"){
+            return false;
+        } 
+    }
+    return true;
+}
 
 module.exports = router;
